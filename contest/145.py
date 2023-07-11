ae155ce98a0db3bd77451b3311dc2111d92b8ec4
3epiossi <3epiossi@gmail.com>
2023-07-11 15:50:14 +0800

    讚讚，擊敗97%的人

---
+++ contest/145.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root == None:
+            return []
+        stack = deque()
+        def dfs(cur):
+            if cur == None:
+                return
+            stack.appendleft(cur.val)
+            dfs(cur.right)
+            dfs(cur.left)
+        dfs(root)
+        return stack
