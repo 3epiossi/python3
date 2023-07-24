7f617c4c18f776001f42f4b214917a448cf2fdd3
3epiossi <3epiossi@gmail.com>
2023-07-24 15:08:26 +0800

    花了一個上午， 還是喜吞timelimit

---
+++ contest/1011-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        length = len(weights)
+        goal = ceil(sum(weights)/days)
+        cache = {}
+        
+        while True:
+            remainDay = days
+            l, r = -1, 0
+            interval = 0
+            val, jump = cache.get(r, (10**7, -1))
+            if val <= goal:
+                interval += val
+                r = jump+1
+            while l < length and remainDay >= -1:
+                while r < length and interval+weights[r] <= goal:
+                    interval += weights[r]
+                    r += 1
+                    val, jump = cache.get(r, (10**7, -1))
+                    if interval+val <= goal:
+                        interval += val
+                        r = jump+1
+                if interval != 0:
+                    cache[l] = (interval, r-1)
+                remainDay -= 1
+                l = r
+                interval = 0
+
+            if remainDay >= 0:
+                return goal
+            goal += 1
