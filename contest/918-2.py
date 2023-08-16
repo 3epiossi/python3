9760b9ed16c7954c1c0138c4361ea07001ea9539
3epiossi <3epiossi@gmail.com>
2023-08-16 11:20:58 +0800

    差點放棄， 但是居然還是被我想出來了

---
+++ contest/918-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        maxRes = -math.inf
+        minSubt = 0
+        prefix = 0
+        nPrefix = 0
+        total = 0
+        for num in nums:
+            minSubt = min(nPrefix+num, minSubt)
+            nPrefix = min(nPrefix+num, 0)
+            maxRes = max(prefix+num, maxRes)
+            prefix = max(0, prefix+num)
+            total += num
+        
+        if minSubt < 0 and total-minSubt > maxRes and total != minSubt:
+            return total-minSubt
+        else:
+            return maxRes
