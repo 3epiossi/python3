94bfeb6542628a3ea7aa703e66906b8159a3c70f
3epiossi <3epiossi@gmail.com>
2023-08-19 08:53:15 +0800

    我就知道， 每次使用dfs， 都會吃tlm

---
+++ contest/946.py
@@ -0,0 +1,18 @@
+class Solution:
+    def validateStackSequences(self, pushed: list[int], popped: list[int]) -> bool:
+        def dfs(rest, stack, result):
+            if len(result) == len(popped):
+                return True if result == popped else False
+            
+            outcome = False
+            if stack:
+                result.append( stack.pop() )
+                outcome = outcome or dfs( rest, stack, result )
+                stack.append( result.pop() )
+            if rest:
+                stack.append( rest.pop(0) )
+                outcome  = outcome or dfs( rest, stack, result )
+                if outcome == True: return True
+                rest.insert( 0, stack.pop() )
+            return outcome
+        return dfs( pushed, [], [])
