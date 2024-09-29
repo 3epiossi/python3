dc29652b0cc33e57f0745cf26687727772da9746
3epiossi <3epiossi@gmail.com>
2024-09-29 22:23:58 +0800

    update

--- contest/1584.py
+++ contest/1584.py
@@ -1,23 +1,22 @@
-class Solution:
-    def minCostConnectPoints(self, points: list[list[int]]) -> int:
-        from collections import defaultdict, deque
+class solution:
+    def mincostconnectpoints(self, points: list[list[int]]) -> int:
+        from collections import defaultdict
+        from heapq import heappush
         cache = defaultdict(list)
         n = len(points)
         for i in range(n-1):
             for j in range(i+1, n):
                 dist = abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1])
-                cache[i].append((j , dist))
-                cache[j].append((i , dist))
-        pas = set([0])
+                cache[i].append((dist, j))
+                cache[j].append((dist, i))
         ret = 0
-        while len(pas) < n:
-            minIx, minVal = 2**31-1, 2**31 - 1
-            for front in pas:
-                for son , val in cache[front]:
-                    if son not in pas:
-                        if val < minVal :
-                            minIx = son
-                            minVal = val
-            ret += minVal
-            pas.add(minIx)
+        minheap = [[0,0]]
+        stpass = set([0])
+        while len(stpass) < n:
+            curcost, cur = minheap.pop()
+            stpass.add(cur)
+            ret += curcost
+            for frontcost, front in cache[cur]:
+                if front not in stpass:
+                    heappush(minheap, [frontcost, front])
         return ret
